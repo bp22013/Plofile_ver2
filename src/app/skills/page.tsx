b6518d76d3e1d42b3fada49a7689a02ae468a244
BP22013 <bp22013@shibaura-it.ac.jp>
2025-08-24 23:37:40 +0900

    first commit

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,13 @@
+/* スキル表示ページ */
+
+'use client';
+
+import React from 'react';
+import { NextPage } from 'next';
+import { AnimatedLeaves } from '../components/leafAnimation';
+
+const SkillsPage: NextPage = () => {
+    return <AnimatedLeaves />;
+};
+
+export default SkillsPage;
